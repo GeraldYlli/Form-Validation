5fc5eaddf00582b80dd3a3f7cbc447e934564852
GeraldYlli <60755196+GeraldYlli@users.noreply.github.com>
2020-12-16 11:04:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [firstNameErr, setFirstNameErr] = useState({});
+  const [lastNameErr, setLastNameErr] = useState({});
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const isValid = formValidation();
+    if (isValid) {
+      //send this data to your backend
+      //restart submit
+      setFirstName("");
+      setLastName("");
+    }
+  };
+
+  const formValidation = () => {
+    const firstNameErr = {};
+    const lastNameErr = {};
+    let isValid = true;
+
+    if (firstName.trim().length < 5) {
+      firstNameErr.firstNameShort = "First Name is too short";
+      isValid = false;
+    }
+    if (firstName.trim().length > 10) {
+      firstNameErr.firstNameLong = "First Name is too long";
+      isValid = false;
+    }
+
+    if (!lastName.includes(",")) {
+      lastNameErr.lastName123 = "Last Name mmust have ,";
+      isValid = false;
+    }
+
+    setFirstNameErr(firstNameErr);
+    setLastNameErr(lastNameErr);
+    return isValid;
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <label>First Name:</label>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      ></input>
+      <br />
+      {Object.keys(firstNameErr).map((key) => {
+        return <div style={{ color: "red" }}>{firstNameErr[key]}</div>;
+      })}
+      <label>Last Name:</label>
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      ></input>
+      <br />
+      {Object.keys(lastNameErr).map((key) => {
+        return <div style={{ color: "red" }}>{lastNameErr[key]}</div>;
+      })}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default App;
